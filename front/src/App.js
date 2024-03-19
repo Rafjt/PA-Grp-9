99535c82d29a3422bae7cfd7c43e04b5ced79d89
Rafael FERNANDO <rafael.fernando@jobteaser.com>
2024-03-19 10:05:55 +0100

    save pre switch

--- front/src/App.js
+++ front/src/App.js
@@ -1,18 +1,19 @@
 import './App.css';
 import React from 'react';
 import Header from './components/header.js';
-import BackOffice from './backOffice.js'; 
+import GestionUtilisateur from './gestionUtilisateur.js'; 
+import BackOffice from './backOffice.js';
 import Footer from './components/footer.js';
 import './App.css';
 import Acceuil from './pages/acceuil.js';
 
 function App() {
-  return (
-    <div className='Page'>
-      <div className="header">
-        <Header />
-      </div>
-      <div className="app">
+    return (
+      <div className='Page'>
+        <div className="header">
+          <Header />
+        </div>
+        <div className="app">
         <Acceuil /> {/* use BackOffice */}
       </div>
       <div className="footer">
