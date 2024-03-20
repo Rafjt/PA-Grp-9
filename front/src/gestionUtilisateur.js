123687dd76fd3ce29b8be111520d6493fdb9d501
Rafael FERNANDO <rafael.fernando@jobteaser.com>
2024-03-20 15:26:50 +0100

    gestion utlilisateurs 60%

--- front/src/gestionUtilisateur.js
+++ front/src/gestionUtilisateur.js
@@ -1,49 +1,76 @@
 import React, { useState, useEffect } from 'react';
-import './gestionUtilisateur.css'
+import './gestionUtilisateur.css';
 
 const GestionUtilisateur = () => {
-    const [users, setUsers] = useState([]);
+    const [users, setUsers] = useState({
+        voyageurs: [],
+        clientsbailleurs: [],
+        prestataires: [],
+    });
+    const [searchTerm, setSearchTerm] = useState('');
+
+    const idFields = {
+        voyageurs: 'ID_Voyageur',
+        clientsbailleurs: 'ID_ClientBailleur',
+        prestataires: 'ID_Prestataire',
+    };
 
     useEffect(() => {
         fetch('http://localhost:3001/api/users')
-            .then(response => response.json())
-            .then(data => {
-                console.log('here:',data);  
+            .then((response) => response.json())
+            .then((data) => {
+                console.log('here:', data);
                 setUsers(data);
             })
-            .catch(error => console.log(error));
+            .catch((error) => console.log(error));
     }, []);
 
-    const handleDelete = (userId) => {
-        
-    };
+    const handleDelete = (userId) => {};
 
-    const handleModify = (userId) => {
-        
+    const handleModify = (userId) => {};
+
+    const handleSearch = (event) => {
+        setSearchTerm(event.target.value);
     };
 
     return (
         <div className="gestionUtilisateur">
-            <div className='greet'>
+            <div className="greet">
                 <h1>Gestion des Utilisateurs</h1>
-                <h2>Vous pouvez ajouter, modifier ou supprimer des utilisateurs</h2>
+                <h2>Ici, vous pouvez rechercher, ajouter, modifier ou supprimer des utilisateurs</h2>
             </div>
-            <div className="users">
-                {users.map(user => (
-                    <div key={user.ID_Voyageur} className="user">
-                        <p>ID: {user.ID_Voyageur}</p>
-                        <p>Nom: {user.Nom}</p>
-                        <p>Prénom: {user.Prenom}</p>
-                        <p>Adresse Email: {user.AdresseMail}</p>
-                        <p>Mot de Passe: {user.MotDePasse}</p>
-                        <p>Admin: {user.Admin}</p>
-                        <button onClick={() => handleDelete(user.ID_Voyageur)}>Supprimer</button>
-                        <button onClick={() => handleModify(user.ID_Voyageur)}>Modifier</button>
+            <input type="text" placeholder="Rechercher (Nom/prénom)" onChange={handleSearch} />
+            <div className="usersContainer">
+                {['voyageurs', 'clientsbailleurs', 'prestataires'].map((userType) => (
+                    <div key={userType}>
+                        <h2>{userType}</h2>
+                        {users[userType]
+                            .filter(
+                                (user) =>
+                                    user.Nom.toLowerCase().includes(searchTerm.toLowerCase()) ||
+                                    user.Prenom.toLowerCase().includes(searchTerm.toLowerCase())
+                            )
+                            .map((user) => (
+                                <div key={user[idFields[userType]]} className="user">
+                                    <p>ID: {user[idFields[userType]]}</p>
+                                    <p>Nom: {user.Nom}</p>
+                                    <p>Prénom: {user.Prenom}</p>
+                                    <p>Adresse Email: {user.AdresseMail}</p>
+                                    <p>Mot de Passe: {user.MotDePasse}</p>
+                                    <p>Admin: {user.Admin}</p>
+                                    <button onClick={() => handleDelete(user[idFields[userType]])}>
+                                        Supprimer
+                                    </button>
+                                    <button onClick={() => handleModify(user[idFields[userType]])}>
+                                        Modifier
+                                    </button>
+                                </div>
+                            ))}
                     </div>
                 ))}
             </div>
         </div>
     );
-}
+};
 
 export default GestionUtilisateur;
