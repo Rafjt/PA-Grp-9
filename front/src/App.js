3a13dc15a9461361eca4fdde98d254b36a74ee4c
Rafael FERNANDO <rafael.fernando@jobteaser.com>
2024-03-20 15:31:33 +0100

    fix conflict

--- front/src/App.js
+++ front/src/App.js
@@ -1,19 +1,18 @@
 import './App.css';
 import React from 'react';
 import Header from './components/header.js';
-import GestionUtilisateur from './gestionUtilisateur.js'; 
-import BackOffice from './backOffice.js';
+import BackOffice from './backOffice.js'; 
 import Footer from './components/footer.js';
 import './App.css';
 import Acceuil from './pages/acceuil.js';
 
 function App() {
-    return (
-      <div className='Page'>
-        <div className="header">
-          <Header />
-        </div>
-        <div className="app">
+  return (
+    <div className='Page'>
+      <div className="header">
+        <Header />
+      </div>
+      <div className="app">
         <Acceuil /> {/* use BackOffice */}
       </div>
       <div className="footer">
