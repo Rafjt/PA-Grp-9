c1be67ef6768e9a9c93cda13f5f3fb19fb4db0e9
Rafael FERNANDO <rafael.fernando@jobteaser.com>
2024-03-20 15:31:33 +0100

    fix conflict

--- front/src/App.js
+++ front/src/App.js
@@ -1,26 +1,25 @@
 import './App.css';
 import React from 'react';
 import Header from './components/header.js';
-import GestionUtilisateur from './gestionUtilisateur.js'; 
-import BackOffice from './backOffice.js';
+import BackOffice from './backOffice.js'; 
 import Footer from './components/footer.js';
 import './App.css';
 import Acceuil from './pages/acceuil.js';
 
 function App() {
-    return (
-      <div className='Page'>
-        <div className="header">
-          <Header />
-        </div>
-        <div className="app">
-          <GestionUtilisateur /> {/* Update the component usage */}
-        </div>
-        <div className="footer">
-          <Footer/>
-        </div>
+  return (
+    <div className='Page'>
+      <div className="header">
+        <Header />
       </div>
-    );
-  }
+      <div className="app">
+        <Acceuil /> {/* use BackOffice */}
+      </div>
+      <div className="footer">
+      <Footer/>
+      </div>
+    </div>
+  );
+}
 
 export default App;
