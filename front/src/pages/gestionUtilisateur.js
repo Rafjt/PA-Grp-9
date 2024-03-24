48b29e661c468f1a826d88b1827a2995830fc499
Rafael FERNANDO <rafael.fernando@jobteaser.com>
2024-03-24 11:04:38 +0100

    User creation OK

--- front/src/pages/gestionUtilisateur.js
+++ front/src/pages/gestionUtilisateur.js
@@ -7,6 +7,43 @@ const GestionUtilisateur = () => {
         clientsbailleurs: [],
         prestataires: [],
     });
+
+
+    const [form, setForm] = useState({
+        nom: '',
+        prenom: '',
+        adresseMail: '', // Updated property name
+        motDePasse: '', // Updated property name
+        admin: '0',
+        type: 'VOYAGEURS',
+    });
+    
+
+        const handleChange = (e) => {
+            setForm({
+                ...form,
+                [e.target.name]: e.target.value,
+            });
+        };
+
+
+
+        const handleSubmit = async (e) => {
+            e.preventDefault();
+    
+            const response = await fetch('http://localhost:3001/api/users', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(form),
+            });
+    
+            const data = await response.json();
+            console.log(data);
+        };
+
+
     const [searchTerm, setSearchTerm] = useState('');
 
     const idFields = {
@@ -43,48 +80,100 @@ const GestionUtilisateur = () => {
 
     const handleModify = (userId) => {};
 
+
+
     const handleSearch = (event) => {
         setSearchTerm(event.target.value);
     };
 
     return (
-        <div className="gestionUtilisateur">
-            <div className="greet">
-                <h1>Gestion des Utilisateurs</h1>
-                <h2>Ici, vous pouvez rechercher, ajouter, modifier ou supprimer des utilisateurs</h2>
-            </div>
-            <input type="text" placeholder="Rechercher (Nom/prénom)" onChange={handleSearch} />
-            <div className="usersContainer">
-                {['voyageurs', 'clientsbailleurs', 'prestataires'].map((userType) => (
-                    <div key={userType}>
-                        <h2>{userType}</h2>
-                        {users[userType]
-                            .filter(
-                                (user) =>
-                                    user.Nom.toLowerCase().includes(searchTerm.toLowerCase()) ||
-                                    user.Prenom.toLowerCase().includes(searchTerm.toLowerCase())
-                            )
-                            .map((user) => (
-                                <div key={user[idFields[userType]]} className="user">
-                                    <p>ID: {user[idFields[userType]]}</p>
-                                    <p>Nom: {user.Nom}</p>
-                                    <p>Prénom: {user.Prenom}</p>
-                                    <p>Adresse Email: {user.AdresseMail}</p>
-                                    <p>Mot de Passe: {user.MotDePasse}</p>
-                                    <p>Admin: {user.Admin}</p>
-                                    <button onClick={() => handleDelete(user[idFields[userType]], userType)}>
-                                        Supprimer
-                                    </button>
-                                    <button onClick={() => handleModify(user[idFields[userType]])}>
-                                        Modifier
-                                    </button>
-                                </div>
-                            ))}
+                <div className="gestionUtilisateur">
+                    <div className="greet">
+                        <h1>Gestion des Utilisateurs</h1>
+                        <h2>Ici, vous pouvez rechercher, ajouter, modifier ou supprimer des utilisateurs</h2>
                     </div>
-                ))}
-            </div>
-        </div>
-    );
-};
+                    <div className="createUser">
+                        <h2>Ajouter un utilisateur</h2>
+                        <form onSubmit={handleSubmit} >
+                            <input
+                                type='text'
+                                placeholder='Nom'
+                                name='nom'
+                                value={form.nom}
+                                onChange={handleChange}
+                            />
+                            <input
+                                type='text'
+                                placeholder='Prénom'
+                                name='prenom'
+                                value={form.prenom}
+                                onChange={handleChange}
+                            />
+                            <input
+                                type='email'
+                                placeholder='Adresse mail'
+                                name='adresseMail'
+                                value={form.adresseMail}
+                                onChange={handleChange}
+                            />
+                            <input
+                                type='text'
+                                placeholder='Mot de passe'
+                                name='motDePasse'
+                                value={form.motDePasse}
+                                onChange={handleChange}
+                            />
+                            <br />
+                            <label htmlFor="admin">Admin :</label>
+                            <select name="admin" id="admin" value={form.admin} onChange={handleChange}>
+                                <option value="0">Non</option>
+                                <option value="1">Oui</option>
+                            </select>
+                            <br />
+                            <br />
+                            <label htmlFor="type">Type :</label>
+                            <select name="type" id="type" value={form.type} onChange={handleChange}>
+                                <option value="VOYAGEURS">Voyageur</option>
+                                <option value="CLIENTSBAILLEURS">Bailleur</option>
+                                <option value="PRESTATAIRES">Prestataire</option>
+                            </select>
+                            <br />
+                            <input type="submit" value="Créer" className='submit' method='POST'></input>
+                        </form>
+                    </div>
+                    <h2>Rechercher un utilisateur</h2>
+                    <input type="text" placeholder="Rechercher (Nom/prénom)" onChange={handleSearch} />
+                    <div className="usersContainer">
+                        {['voyageurs', 'clientsbailleurs', 'prestataires'].map((userType) => (
+                            <div key={userType}>
+                                <h2>{userType}</h2>
+                                {users[userType]
+                                    .filter(
+                                        (user) =>
+                                            user.Nom.toLowerCase().includes(searchTerm.toLowerCase()) ||
+                                            user.Prenom.toLowerCase().includes(searchTerm.toLowerCase())
+                                    )
+                                    .map((user) => (
+                                        <div key={user[idFields[userType]]} className="user">
+                                            <p>ID: {user[idFields[userType]]}</p>
+                                            <p>Nom: {user.Nom}</p>
+                                            <p>Prénom: {user.Prenom}</p>
+                                            <p>Adresse Email: {user.AdresseMail}</p>
+                                            <p>Mot de Passe: {user.MotDePasse}</p>
+                                            <p>Admin: {user.Admin}</p>
+                                            <button onClick={() => handleDelete(user[idFields[userType]], userType)}>
+                                                Supprimer
+                                            </button>
+                                            <button onClick={() => handleModify(user[idFields[userType]])}>
+                                                Modifier
+                                            </button>
+                                        </div>
+                                    ))}
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            );
+        };
 
-export default GestionUtilisateur;
+        export default GestionUtilisateur;
