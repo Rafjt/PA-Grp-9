5601627258e22ae8997f2e38ea5ef819298d95ae
Polack <romain.polack75@gmail.com>
2024-05-27 22:56:49 +0200

    ajout gestion de table overflow sur la page paiement voilà soyons fou (je suis interné en hopital psychiatrique je suis vraiment fou

--- front/src/pages/gestionPaiement.js
+++ front/src/pages/gestionPaiement.js
@@ -84,15 +84,20 @@ const GestionPaiement = () => {
     });
   };
 
+  const tableContainerStyle = {
+    maxHeight: "600px",
+    overflowY: "auto",
+  };
+
   return (
     <div>
       <h1>Gestion des paiements</h1>
       <button
-            onClick={() => (window.location.href = "/backOffice")}
-            className="back-button"
-            >
-            Retour
-            </button>
+        onClick={() => (window.location.href = "/backOffice")}
+        className="back-button"
+      >
+        Retour
+      </button>
       <div className="creation rounded shadow">
         <h2>Créer un paiement</h2>
         <form onSubmit={handleSubmit}>
@@ -171,77 +176,102 @@ const GestionPaiement = () => {
         />
       </div>
       <div className="usersContainer rounded shadow mt-3">
-        <table className="table table-bordered table-hover custom-table">
-          <thead>
-            <tr>
-              <th className="narrow-column">ID</th>
-              <th>ID de réservation</th>
-              <th>Nom</th>
-              <th>Date de paiement(AAAA-MM-JJ)</th>
-              <th>Méthode de paiement</th>
-              <th>Montant</th>
-              <th>Statut</th>
-              <th>Actions</th>
-            </tr>
-          </thead>
-          <tbody>
-            {filteredPaiements.map((paiement) => (
-              <tr key={paiement.id}>
-                {editingId === paiement.id ? (
-                  <>
-                    <td>{paiement.id}</td>
-                    <td>
-                      <input
-                        type="number"
-                        name="id_Reservation"
-                        value={editingData.id_Reservation}
+        <div className="table-container" style={tableContainerStyle}>
+          <table className="table table-bordered table-hover custom-table">
+            <thead>
+              <tr>
+                <th className="narrow-column">ID</th>
+                <th>ID de réservation</th>
+                <th>Nom</th>
+                <th>Date de paiement(AAAA-MM-JJ)</th>
+                <th>Méthode de paiement</th>
+                <th>Montant</th>
+                <th>Statut</th>
+                <th>Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              {filteredPaiements.map((paiement) => (
+                <tr key={paiement.id}>
+                  {editingId === paiement.id ? (
+                    <>
+                      <td>{paiement.id}</td>
+                      <td>
+                        <input
+                          type="number"
+                          name="id_Reservation"
+                          value={editingData.id_Reservation}
+                          onChange={handleEditingChange}
+                        />
+                      </td>
+                      <td>
+                        <input
+                          type="text"
+                          name="nom"
+                          value={editingData.nom}
+                          onChange={handleEditingChange}
+                        />
+                      </td>
+                      <td>
+                        <input
+                          type="date"
+                          name="datePaiement"
+                          value={editingData.datePaiement}
+                          onChange={handleEditingChange}
+                        />
+                      </td>
+                      <td>
+                        <input
+                          type="text"
+                          name="methodePaiement"
+                          value={editingData.methodePaiement}
+                          onChange={handleEditingChange}
+                        />
+                      </td>
+                      <td>
+                        <input
+                          type="number"
+                          name="montant"
+                          value={editingData.montant}
+                          onChange={handleEditingChange}
+                        />
+                      </td>
+                      <select
+                        id="statut"
+                        name="statut"
+                        value={editingData.statut}
                         onChange={handleEditingChange}
-                      />
-                    </td>
-                    <td>
-                      <input
-                        type="text"
-                        name="nom"
-                        value={editingData.nom}
-                        onChange={handleEditingChange}
-                      />
-                    </td>
-                    <td>
-                      <input
-                        type="date"
-                        name="datePaiement"
-                        value={editingData.datePaiement}
-                        onChange={handleEditingChange}
-                      />
-                    </td>
-                    <td>
-                      <input
-                        type="text"
-                        name="methodePaiement"
-                        value={editingData.methodePaiement}
-                        onChange={handleEditingChange}
-                      />
-                    </td>
-                    <td>
-                      <input
-                        type="number"
-                        name="montant"
-                        value={editingData.montant}
-                        onChange={handleEditingChange}
-                      />
-                    </td>
-                    <select
-                      id="statut"
-                      name="statut"
-                      value={editingData.statut}
-                      onChange={handleEditingChange}
-                    >
-                      <option value="En attente">En attente</option>
-                      <option value="Validé">Validé</option>
-                    </select>
-                    <td>
-                      <div className="button-container">
-                        <button onClick={handleAccept}>Accepter</button>
+                      >
+                        <option value="En attente">En attente</option>
+                        <option value="Validé">Validé</option>
+                      </select>
+                      <td>
+                        <div className="button-container">
+                          <button onClick={handleAccept}>Accepter</button>
+                          <button onClick={() => handleDelete(paiement.id)}>
+                            Supprimer
+                          </button>
+                          {paiement.statut !== "Validé" ? (
+                            <button onClick={() => handleValidate(paiement.id)}>
+                              Valider
+                            </button>
+                          ) : null}
+                        </div>
+                      </td>
+                    </>
+                  ) : (
+                    <>
+                      <td>{paiement.id}</td>
+                      <td>{paiement.id_Reservation}</td>
+                      <td>{paiement.nom}</td>
+                      <td>{paiement.datePaiement}</td>
+                      <td>{paiement.methodePaiement}</td>
+                      <td>{paiement.montant} €</td>
+                      <td>{paiement.statut}</td>
+                      <td>
+                        <button onClick={() => handleEdit(paiement)}>
+                          Modifier
+                        </button>
                         <button onClick={() => handleDelete(paiement.id)}>
                           Supprimer
                         </button>
@@ -250,37 +280,14 @@ const GestionPaiement = () => {
                             Valider
                           </button>
                         ) : null}
-                      </div>
-                    </td>
-                  </>
-                ) : (
-                  <>
-                    <td>{paiement.id}</td>
-                    <td>{paiement.id_Reservation}</td>
-                    <td>{paiement.nom}</td>
-                    <td>{paiement.datePaiement}</td>
-                    <td>{paiement.methodePaiement}</td>
-                    <td>{paiement.montant} €</td>
-                    <td>{paiement.statut}</td>
-                    <td>
-                      <button onClick={() => handleEdit(paiement)}>
-                        Modifier
-                      </button>
-                      <button onClick={() => handleDelete(paiement.id)}>
-                        Supprimer
-                      </button>
-                      {paiement.statut !== "Validé" ? (
-                        <button onClick={() => handleValidate(paiement.id)}>
-                          Valider
-                        </button>
-                      ) : null}
-                    </td>
-                  </>
-                )}
-              </tr>
-            ))}
-          </tbody>
-        </table>
+                      </td>
+                    </>
+                  )}
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
   );
