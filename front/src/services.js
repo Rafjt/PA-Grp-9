319ddce2dccb2455d668c5d51ec4ac2be8137048
Rafael FERNANDO <rafael.fernando@jobteaser.com>
2024-06-22 23:27:43 +0200

    je pousse

--- front/src/services.js
+++ front/src/services.js
@@ -8,7 +8,7 @@ export const URL_ENVOI_MAIL = `${BASE_URL}/mail`;
 export const URL_ABONNEMENT = `${BACK_URL}/abonnement`;
 export const URL_RESERVATION = `${BASE_URL}/reservation`;
 export const URL_AUTH = `${BACK_URL}/auth`;
-
+// proutcacchoiassemerde
 export const fetchUsers = async () => {
   try {
     const response = await fetch(URL_USERS);
